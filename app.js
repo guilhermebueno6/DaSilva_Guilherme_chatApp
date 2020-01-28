e2dcdc2679ef1f6d44a834b7b6b249236d8421d5
Da Silva <g_dasilva92543@ats-hr420-dm28.fc.ca>
2020-01-28 15:45:23 -0500

    first sprint, made connection and disconnection socket triggers

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+//import the socket.io
+const io = require('socket.io')();
+// instantiate the socket.io library right away with the () method -> makes it run 
 
 const port = process.env.PORT || 3030;
 
@@ -13,3 +16,17 @@ app.get('/', (req, res) => {
 const server = app.listen(port, () => {
     console.log(`app is running on port ${port}`);
 });
+
+// this is all of our socket.io messaging functionality
+
+//atach socket.io 
+io.attach(server);
+
+io.on('connection', function(socket) {
+    console.log('user connected');
+
+    //listen for a disconnection
+    socket.on('disconnect', function() {
+        console.log('a user disconnected');
+    })
+})
